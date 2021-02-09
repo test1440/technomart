b6e14a58100c91cd61d75cdbfd747e710b15a1ee
test1440 <den.grantsb@gmail.com>
2021-02-09 20:23:31 +0300

    оптимизация js

--- source/js/popups.js
+++ source/js/popups.js
@@ -1,70 +1,51 @@
-/* var */
 var enter = document.querySelector(".enter");
-var modal_authorization = document.querySelector(".modal-authorization");
-var button_close_authorization = document.querySelector(
-  ".modal-authorization > .button-close"
-);
-var login = modal_authorization.querySelector("[name=username]");
+if (enter != null) {
+  var modal_authorization = document.querySelector(".modal-authorization");
+  var button_close_authorization = document.querySelector(
+    ".modal-authorization > .button-close"
+  );
+
+  popup(enter, modal_authorization, button_close_authorization);
+}
 
 var map = document.querySelector(".map");
-var modal_map = document.querySelector(".modal-map");
-var button_close_map = document.querySelector(".modal-map > .button-close");
+if (map != null) {
+  var modal_map = document.querySelector(".modal-map");
+  var button_close_map = document.querySelector(".modal-map > .button-close");
+  popup(map, modal_map, button_close_map);
+}
 
-var cart = document.querySelector(".button-buy");
-var modal_add_cart = document.querySelector(".modal-add-cart");
-var button_close_cart = document.querySelector(
-  ".modal-add-cart > .button-close"
-);
+var cart = document.querySelectorAll(".button-buy");
+if (cart != null) {
+  var modal_add_cart = document.querySelector(".modal-add-cart");
+  var button_close_cart = document.querySelector(
+    ".modal-add-cart > .button-close"
+  );
+  cart.forEach((element) => popup(element, modal_add_cart, button_close_cart));
+}
 
 var feedback = document.querySelector(".button-feedback");
-var modal_feedback = document.querySelector(".modal-feedback");
-var button_close_feedback = document.querySelector(
-  ".modal-feedback > .button-close"
-);
-var feedfocus = modal_feedback.querySelector("[name=your_name]");
+if (feedback != null) {
+  var modal_feedback = document.querySelector(".modal-feedback");
+  var button_close_feedback = document.querySelector(
+    ".modal-feedback > .button-close"
+  );
+  popup(feedback, modal_feedback, button_close_feedback);
+}
 
-/* authorization */
-enter.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_authorization.classList.add("modal-show");
-  login.focus();
-});
+function popup(open, modal, close) {
+  open.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.add("modal-show");
+    var form = modal.getElementsByTagName("form")[0];
+    if (form) {
+      var firstInput = form.getElementsByTagName("input")[0];
+      firstInput.focus();
+    }
+  });
 
-button_close_authorization.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_authorization.classList.remove("modal-show");
-});
-
-/* map */
-map.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_map.classList.add("modal-show");
-});
-
-button_close_map.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_map.classList.remove("modal-show");
-});
-
-/* cart */
-cart.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_add_cart.classList.add("modal-show");
-});
-
-button_close_cart.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_add_cart.classList.remove("modal-show");
-});
-
-/* feedback */
-feedback.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_feedback.classList.add("modal-show");
-  feedfocus.focus();
-});
-
-button_close_feedback.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal_feedback.classList.remove("modal-show");
-});
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal-show");
+  });
+}
